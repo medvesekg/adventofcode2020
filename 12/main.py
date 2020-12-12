db183b7d68d6bb3fad5a8ef23b20e9ca21136ce3
Gregor Medvesek <medvesek.gregor@gmail.com>
2020-12-12 11:55:27 +0100

    Day 12 part 2

--- 12/main.py
+++ 12/main.py
@@ -1,3 +1,5 @@
+import math
+
 with open('input', 'r') as f:
     lines = [line.strip() for line in f.readlines()]
 
@@ -60,27 +62,41 @@ def manhattan(ship):
     return abs(ship['X']) + abs(ship['Y'])
 
 
-# def run2(start_state, start_waypoint, instructions):
-#     ship = start_state
-#     waypoint = start_waypoint
-#
-#     for instruction in instructions:
-#         code = instruction['code']
-#         value = instruction['value']
-#         if code == 'F':
-#             ship['X'] += (waypoint['X'] * value)
-#             ship['Y'] += (waypoint['Y'] * value)
-#         else:
-#             instruction_map['code']
+def rotate_waypoint(waypoint, direction, degrees):
+    radians = degrees * (math.pi / 180)
+    radians = radians if direction == 'R' else -radians
+
+    return {
+        'X': round((math.cos(radians) * waypoint['X']) + math.sin(radians) * waypoint['Y']),
+        'Y': round((math.cos(radians) * waypoint['Y']) - math.sin(radians) * waypoint['X'])
+    }
+
+def run2(start_state, start_waypoint, instructions):
+    ship = start_state
+    waypoint = start_waypoint
+
+    for instruction in instructions:
+        code = instruction['code']
+        value = instruction['value']
+        if code == 'F':
+            ship['X'] += (waypoint['X'] * value)
+            ship['Y'] += (waypoint['Y'] * value)
+        elif code in ['R', 'L']:
+            waypoint = rotate_waypoint(waypoint, code, value)
+        else:
+            value_function = instruction_map[code]['value']
+            attribute = instruction_map[code]['attribute']
+            waypoint[attribute] += value_function(value)
+
+    return ship
 
 
 
 end_state1 = run({ 'facing': 0, 'Y': 0, 'X': 0,}, instructions)
-
 print(manhattan(end_state1))
 
-# end_state2 = run2({ 'facing': 0, 'Y': 0, 'X': 0,}, {'Y': 1, 'X': 10,}, instructions)
-#print(manhattan(end_state2))
+end_state2 = run2({ 'facing': 0, 'Y': 0, 'X': 0,}, {'Y': 1, 'X': 10,}, instructions)
+print(manhattan(end_state2))
 
 
 
